3eb1a0d553cd1e41f93da9ed17caa32ef27e17b5
Dušan Osmokrović <60246031+d855@users.noreply.github.com>
2022-08-02 20:15:59 +0200

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AdviceBox></AdviceBox>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AdviceBox from "@/components/AdviceBox";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      AdviceBox
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Manrope', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AdviceBox.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "AdviceBox"
+}
+</script>
+
+<template>
+    <h2>Advice box</h2>
+</template>
+
+
+<style scoped>
+
+</style>
