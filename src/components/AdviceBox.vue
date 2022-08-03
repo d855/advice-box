51b26719ca9c2ceb0eaf25b4cf183e122b5edeca
Dušan Osmokrović <60246031+d855@users.noreply.github.com>
2022-08-03 12:51:01 +0200

    desktop design

--- src/components/AdviceBox.vue
+++ src/components/AdviceBox.vue
@@ -5,10 +5,17 @@ export default {
 </script>
 
 <template>
-    <h2>Advice box</h2>
+    <div class="h-screen w-screen grid place-content-center">
+        <div class="relative w-96 h-56 bg-dark-grayish-blue pt-8 rounded-xl px-10">
+            <h2 class="text-neon-green uppercase text-xs font-semibold tracking-widest mb-5">Advice #117</h2>
+            <div>
+                <q class="text-light-cyan">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, vero?</q>
+                <img src="../assets/pattern-divider-desktop.svg" alt="divider" class="mt-5">
+            </div>
+
+            <div class="absolute bg-neon-green w-12 h-12 grid place-content-center left-0 right-0 mx-auto m-0 bottom-0 -mb-5 rounded-full">
+                <img src="../assets/icon-dice.svg" class="w-5 h-5   " alt="dice">
+            </div>
+        </div>
+    </div>
 </template>
-
-
-<style scoped>
-
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        letterSpacing: {
+          widest: '.25em'
+        },
+        extend: {
+            colors: {
+                'light-cyan': 'hsl(193, 38%, 86%)',
+                'neon-green': 'hsl(150, 100%, 66%)',
+                'grayish-blue': 'hsl(217, 19%, 38%)',
+                'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+                'dark-blue': 'hsl(218, 23%, 16%)'
+            },
+            spacing: {
+                desktop: '90rem'
+            }
+        },
+    },
+    plugins: [],
 }
